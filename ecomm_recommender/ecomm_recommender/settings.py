d89c3f7185fc806f1a9c698b7068dc2adf333cc5
AliyuAlbako <alirilwan03@gmail.com>
2025-09-26 23:33:52 -0700

    added procfile and requirements.txt files for deployment

--- ecomm_recommender/ecomm_recommender/settings.py
+++ ecomm_recommender/ecomm_recommender/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
      "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
